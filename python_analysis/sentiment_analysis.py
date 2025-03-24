f084987b6789bf15a57be289a8f7a5091128d916
Abdu1Hak <158542567+Abdu1Hak@users.noreply.github.com>
2025-03-24 00:26:03 -0400

    Removed the comments from html files

--- python_analysis/sentiment_analysis.py
+++ python_analysis/sentiment_analysis.py
@@ -39,27 +39,3 @@ class SentimentAnalysis:
         return int(scam_percent), categorical_scores
 
 
-
-"""
-CODE EXPLANATION:
-
-- Used transformers and a specific model to return the presence of 6 of the most common emotions prevelant in Scam Messages
-- Using research and statistical Data, I assigned weights to each category:
-    - Reasoning ->
-        - Fear is the Primary Scam Emotion (invoke urgency and threats): 0.45
-        - Surprise is the second highest, creating a sense of anxiety: 0.25
-        - Anger is used mildly in threats/coercions: 0.15
-        - Joy are rarely found in the too good to be true offers: 0.10
-        - Sadness is almost never found in correlation with scams: -0.10
-        - Love is the most overlapping factor in all message types: -0.15
-
-- Now the max possible score is 0.95 (if only positive emotions are present) and the minimum is -0.25 (if negatives are only present)
-- After retrieving the sum of all the scores present, it is normalized by dividing against the maximum existing possibility
-- Finally in the case that a negative score is returned, we return 0 instead. 
-
-
-- An emotional analysis isnt nearly accurate nor comprehensive indicator of a scam message, however it offers a few benefits:
-    - Informs us the exact abundance of strong emotions
-    - Useful decider when it comes to edge cases
-    - Only contributes 10-15% to the Final Probability Score
-"""
